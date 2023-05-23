9ee6954b0972f54c895ea16bf034d5928c97f2df
Amro Gad <amrelsayedrabie@gmail.com>
2023-05-23 14:45:28 +0300

    added firebase app added register view model change package name added provider added function to create user in firebase

--- android/app/src/main/kotlin/com/amro/xenochat/MainActivity.kt
+++ android/app/src/main/kotlin/com/amro/xenochat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.xeno_chat
+package com.amro.xenochat
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/views/widgets/xeno_textfield.dart
+++ lib/views/widgets/xeno_textfield.dart
@@ -4,14 +4,22 @@ class XenoTextField extends StatelessWidget {
   String? title;
   String? Function(String?)? validator;
   String? value;
-  XenoTextField({this.title, this.validator, super.key});
+  bool obscureText;
+  TextEditingController controller;
+  XenoTextField({this.title,
+    required this.controller,
+    required this.validator,
+    this.obscureText = false,
+    super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         labelText: title,
       ),
+      obscureText: obscureText,
       validator: validator,
     );
   }
